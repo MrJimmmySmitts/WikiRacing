6adb8a748a4b36e73c8cd80fbdbe410b035fae19
MrJimmySmitts <james.smith5@education.vic.gov.au>
2024-11-22 09:31:41 +1100

    new timer version

--- GameTimer.py
+++ GameTimer.py
@@ -1,43 +1,41 @@
 from PyQt5 import QtCore
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QWidget, QMainWindow, QLCDNumber, QVBoxLayout, QMessageBox, QApplication, QPushButton
+from PyQt5.QtCore import QTimer, QTime
+from PyQt5.QtWidgets import QLCDNumber, QApplication, QPushButton
 from sys import argv, exit
-from datetime import datetime
 
-class TimerWindow(QWidget):
+
+class TimerWidget(QLCDNumber):
     def __init__(self, parent=None):
-        super().__init__(parent)
-        QMainWindow.__init__(self, parent)
+        super(TimerWidget, self).__init__(parent)
 
-        self.resize(800, 600)
+        self.setWindowTitle("Timer Widget")
+        self.resize(800,600)
+        self.setSegmentStyle(QLCDNumber.Filled)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.showTime)
+        self.timer.start(10)
+        self.showTime()
 
-        self.lcd = QLCDNumber(self)
-        self.lcd.setNumDigits(8)
-        self.timer = QTimer(self.lcd)
-        #self.timer.timeout.connect(self.lcd_number)
-        #self.lcd_number()
-        self.currentTime = QTime(0, 0, 0)
-        QVBoxLayout(self).addWidget(self.lcd)
-        self.timer.start(1000)
         self.startButton = QPushButton(self)
         self.startButton.setText("Start")
         self.startButton.setGeometry(QtCore.QRect(150, 450, 200, 50))
-        #self.startButton.clicked.connect(self.initTimer())
-        self.startButton = QPushButton(self)
-        self.startButton.setText("Stop")
-        self.startButton.setGeometry(QtCore.QRect(450, 450, 200, 50))
-        #self.startButton.clicked.connect(self.timer.stop())
+        self.startButton.clicked.connect(self.timer.start)
+        self.stopButton = QPushButton(self)
+        self.stopButton.setText("Stop")
+        self.stopButton.setGeometry(QtCore.QRect(450, 450, 200, 50))
+        self.stopButton.clicked.connect(self.timer.stop)
 
-    '''def lcd_number(self):
-        time = datetime.now()
-        formatted_time = time.strftime("%M:%S")
-        self.lcd.setDigitCount(6)
-        self.lcd.display(formatted_time)
-'''
+    def showTime(self):
+        time = QTime(0,0,0)
+        timeDisplay = time.toString('mm:ss')
+        self.display(timeDisplay)
+
+
+
 if __name__ == '__main__':
     app=QApplication(argv)
-    frame=TimerWindow()
+    frame = TimerWidget()
     frame.show()
     frame.raise_()
     exit(app.exec_())
