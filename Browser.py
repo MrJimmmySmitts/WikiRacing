8e234d8e0184fee4cb738533a0f807d8ad9cb93c
bruceesmith <bruceesmith@gmail.com>
2025-01-19 20:40:14 +1100

    Fetch actual random targets and display the actual random goal in the MainWindow

--- Browser.py
+++ Browser.py
@@ -10,6 +10,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
+import urllib.request
 
 
 '''
@@ -17,6 +18,8 @@ Name: MainWindow
 Attributes: browser, status, toolbar
 Methods: add_to_toolbar, update_title, set_url
 '''
+
+
 class MainWindow(QMainWindow):
     # Initialise Browser window
     def __init__(self, start, goal, parent=None):
@@ -33,9 +36,7 @@ class MainWindow(QMainWindow):
         # Central widget with QGraphicsView as the root and a QVBoxLayout
         self.central = QGraphicsView()
         self.vBox = QVBoxLayout()
-        
-        
-        
+
         self.lcd = self.add_timer()
         self.vBox.addWidget(self.lcd)
         self.endPageLabel = QLabel("Your Goal Is: " + goal)
@@ -50,9 +51,9 @@ class MainWindow(QMainWindow):
         self.central.setLayout(self.vBox)
         self.central.setMinimumHeight(800)
         self.central.setMinimumWidth(800)
-        self.central.resize(800,800)
+        self.central.resize(800, 800)
         self.setCentralWidget(self.central)
-                
+
         # Initialising a status bar object
         self.status = QStatusBar(self)
         self.setStatusBar(self.status)
@@ -61,7 +62,8 @@ class MainWindow(QMainWindow):
         self.toolbar = QToolBar("Navigation")
         self.addToolBar(self.toolbar)
         self.add_to_toolbar("Back", "Back to previous page", self.browser.back)
-        self.add_to_toolbar("Next", "Forward to next page", self.browser.forward)
+        self.add_to_toolbar("Next", "Forward to next page",
+                            self.browser.forward)
         self.add_to_toolbar("Reload", "Reload page", self.browser.reload)
         self.add_to_toolbar("Stop", "Stop loading page", self.browser.stop)
 
@@ -78,6 +80,7 @@ class MainWindow(QMainWindow):
     tip - the tooltip that displays on hover
     action - the function to be performed on triggered
     '''
+
     def add_to_toolbar(self, text, tip, action):
         button = QAction(text, self)
         button.setStatusTip(tip)
@@ -88,6 +91,7 @@ class MainWindow(QMainWindow):
     Method to create a browser via QTWebEngineView
     Returns a QTWebEngineView object
     '''
+
     def add_browser(self):
         browser = QWebEngineView()
         browser.setPage(CustomWebPage(self))
@@ -96,17 +100,17 @@ class MainWindow(QMainWindow):
         browser.resize(800, 600)
         browser.loadFinished.connect(self.update_title)
         return browser
-    
+
     def add_timer(self):
         lcd = QLCDNumber(self)
         lcd.setNumDigits(8)
         lcd.setSegmentStyle(QLCDNumber.Filled)
-        lcd.resize(600,200)
+        lcd.resize(600, 200)
         self.timer = QTimer()
         self.timer.timeout.connect(self.showTime)
-        self.time = QTime(0,0,0)
+        self.time = QTime(0, 0, 0)
         lcd.display(self.time.toString('mm:ss.zzz'))
-        #self.timer.start(10)
+        # self.timer.start(10)
         return lcd
 
     # Update the QLCDNumber object with current timing
@@ -141,13 +145,14 @@ class MainWindow(QMainWindow):
         return goal
 
 
-
 '''
 Class to restrict navigation to within Wikipedia
 Checks for requests for pages outside of wikipedia as well as 
 search requests made using the inbuilt wikipedia search bar
 If either request is made, the request is ignored
 '''
+
+
 class CustomWebPage(QWebEnginePage):
     def acceptNavigationRequest(self, url,  _type, isMainFrame):
         lower = url.toString().lower()
@@ -155,11 +160,14 @@ class CustomWebPage(QWebEnginePage):
             return False
         return super().acceptNavigationRequest(url,  _type, isMainFrame)
 
+
 '''
 Name: MenuWindow
 Attributes: centralWidget, playBtn, playRandBtn, settingsBtn, quitBtn
 Methods: start_game, startgame_random, run_settings
 '''
+
+
 class MenuWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -175,11 +183,14 @@ class MenuWindow(QMainWindow):
 
         # Initialise buttons
         # Play Button
-        self.add_button(QtCore.QRect(300, 150, 200, 50), "Play", self.start_game)
+        self.add_button(QtCore.QRect(300, 150, 200, 50),
+                        "Play", self.start_game)
         # Play Random Start, Random End Button
-        self.add_button(QtCore.QRect(300, 250, 200, 50), "Play Random", self.start_game_random)
+        self.add_button(QtCore.QRect(300, 250, 200, 50),
+                        "Play Random", self.start_game_random)
         # Start up Timer Widget [Placeholder till functionality is incorporated into MainWindow]
-        self.add_button(QtCore.QRect(300, 350, 200, 50), "Leaderboard", self.show_leaderboard)
+        self.add_button(QtCore.QRect(300, 350, 200, 50),
+                        "Leaderboard", self.show_leaderboard)
         # Quit Game Button
         self.add_button(QtCore.QRect(300, 450, 200, 50), "Quit", self.close)
 
@@ -197,6 +208,7 @@ class MenuWindow(QMainWindow):
     text - string - text to display on button
     action - action to be performed on clicked event
     '''
+
     def add_button(self, rect, text, action):
         button = QPushButton(self.centralWidget)
         button.setGeometry(rect)
@@ -208,6 +220,7 @@ class MenuWindow(QMainWindow):
     NOT IMPLEMENTED: start point, end point, timer, start round, end round
     Random used as a placeholder
     '''
+
     def start_game(self):
         self.window = InputWindow()
         self.close()
@@ -216,6 +229,7 @@ class MenuWindow(QMainWindow):
     Method to begin a random round
     NOT IMPLEMENTED: end point, timer, end round
     '''
+
     def start_game_random(self):
         self.window = CountdownTimer()
         self.window.show()
@@ -227,8 +241,10 @@ class MenuWindow(QMainWindow):
     will have options for different score categories and 
     the ability to return to menu
     '''
+
     def show_leaderboard(self):
         pass
+
 
 '''
 Name: InputWindow
@@ -236,6 +252,8 @@ Description: A small pop-up window that waits for user input
 for the start page and the goal for the game before starting a new game
 option to return to main menu
 '''
+
+
 class InputWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -245,8 +263,10 @@ class InputWindow(QMainWindow):
         self.resize(400, 400)
         self.central = QGraphicsView()
         self.grid = QVBoxLayout()
-        self.start, self.startEntered = QInputDialog.getText(self, "Start Page", "Enter a page to start: ")
-        self.goal, self.goalEntered = QInputDialog.getText(self, "Goal", "Enter a goal: ")
+        self.start, self.startEntered = QInputDialog.getText(
+            self, "Start Page", "Enter a page to start: ")
+        self.goal, self.goalEntered = QInputDialog.getText(
+            self, "Goal", "Enter a goal: ")
         self.startLabel = QLabel("Start Page: " + self.start)
         self.startLabel.setFont(QFont("MS Gothic", 10))
         self.grid.addWidget(self.startLabel)
@@ -270,35 +290,53 @@ class InputWindow(QMainWindow):
         self.window.show()
         self.close()
 
+
 '''
 Name: Countdown Timer [QLCDNumber]
 Description: A small window that will display a time in seconds 
 and count down until it reaches zero, whereupon it will load the main browser page
 '''
+
+
 class CountdownTimer(QLCDNumber):
     def __init__(self, parent=None):
         super(CountdownTimer, self).__init__(parent)
 
         self.setWindowTitle("Get Ready!")
         self.setNumDigits(1)
-        self.resize(100,100)
+        self.resize(100, 100)
         self.setSegmentStyle(QLCDNumber.Filled)
         self.timer = QTimer()
         self.timer.timeout.connect(self.showTime)
-        self.time = QTime(0,0,5)
+        self.time = QTime(0, 0, 5)
         self.display(self.time.toString('s'))
         self.timer.start(1000)
 
     def showTime(self):
         self.time = self.time.addSecs(-1)
         self.display(self.time.toString('s'))
-        if self.time == QtCore.QTime(0,0,0):
-            self.start_game("Special:Random", "Special:Random")
+        if self.time == QtCore.QTime(0, 0, 0):
+            self.start_game(self.special(), self.special())
 
     def start_game(self, start, goal):
         self.window = MainWindow(start, goal)
         self.window.show()
         self.close()
+
+    def special(self):
+        urllib.request.build_opener(NoRedirection)
+        opener = urllib.request.build_opener(NoRedirection)
+        response = opener.open('https://en.wikipedia.org/wiki/Special:Random')
+        location = response.getheader('Location')
+        return location.removeprefix('https://en.wikipedia.org/wiki/')
+
+
+class NoRedirection(urllib.request.HTTPErrorProcessor):
+    def http_response(self, _, response):
+        return response
+    https_response = http_response
+
+
 '''
 Main Loop
 Starts a QApplciation and loads the startMenu
